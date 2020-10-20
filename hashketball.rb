4b3b43c01079c33973f70a62e43dae0ca369efd3
Alexa Botello <abotello20@yahoo.com>
2020-10-20 03:56:11 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -191,8 +191,8 @@ def player_stats(input)
     team_info.each do |key, value|
       if key == :players
         value.each do |player|
-          if input == player[:player_name]
-            player.delete(:player_name) # having player name inside the hash was a bad idea!
+          if input == hash[:player_name]
+            player.delete(:player_name)
             return player
           end
         end
