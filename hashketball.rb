8da09cf028ce4716005aba47efc0f5da6cdb3a92
Alexa Botello <abotello20@yahoo.com>
2020-10-20 03:47:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -156,7 +156,7 @@ away_team = game_hash[:away][:team_name]
 game_hash.each do |bball_team_name|
     if home_team == name_of_team
     return game_hash[:home][:colors]
-    elsif away == name_of_team
+    elsif away_team == name_of_team
        return game_hash[:away][:colors]
     end
   end
